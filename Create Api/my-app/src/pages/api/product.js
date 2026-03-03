a8d3ea732e0334ef61efbf4185072528db997a2b
Zaheer Ali <zaheercode09@gmail.com>
2026-03-03 17:57:37 +0500

    Api Generation

---
+++ Create Api/my-app/src/pages/api/product.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        name: "Iphone",
+        price: 220000,
+
+    }, {
+        id: 2,
+        name: "VIVO",
+        price: 65000,
+
+    }, {
+        id: 3,
+        name: "nokia",
+        price: 4000,
+
+    }
+];
+
+export default function handle(req, res) {
+    if (req.method === "GET") {
+        return res.status(200).json(data);
+    } else {
+        return res.status(404)
+            .json({ message: "This Method is Not Allowed" })
+            .send();
+    }
+}
